84f829d6ec58f2ead7e8b93ef18ce0840a7e669c
Jun Kim <jun.kim@cdk.com>
2022-02-04 23:00:57 -0600

    uploading week 1 assignments

---
+++ uagc/cpt307/CalculatePayProgram.java
@@ -0,0 +1,15 @@
+/**
+ * Week 1 - Assignment
+ * 
+ * Created by: Chongjun Kim
+ * Date: 02-03-2022
+ * Course: CPT307 - Data Structures & Algorithms (INE2206A)
+ */
+
+public class CalculatePayProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world, my name is Chongjun Kim!!"); // output
+    }
+}
---
+++ uagc/cpt307/HelloWorldMyNameIs.java
@@ -0,0 +1,15 @@
+/**
+ * Week 1 - Interactive Assignment 1
+ *
+ * Created by: Chongjun Kim
+ * Date: 02-03-2022
+ * Course: CPT307 - Data Structures & Algorithms (INE2206A)
+ */
+
+public class HelloWorldMyNameIs
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world, my name is Chongjun Kim!!"); // output
+    }
+}
