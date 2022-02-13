dde928d973fadf5e1eea6cc43946ce9c1ac7e2c5
Jun Kim <jun.kim@cdk.com>
2022-02-12 23:13:17 -0600

    created java search program for week 2

---
+++ uagc/cpt307/SearchProgram.java
@@ -0,0 +1,150 @@
+
+/**
+* Week 2 - Assignment
+* 
+* Title: Search Program
+* Created by: Chongjun Kim
+* School: The University of Arizona Global Campus
+* Course: CPT307 - Data Structures & Algorithms (INE2206A)
+* Professor: Michael Hayden
+* Due Date: 02-14-2022
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class SearchProgramFrame extends JFrame implements ActionListener {
+
+    // default components
+    private Container c;
+    private JLabel title;
+    private JLabel searchKey;
+    private JTextField searchKeyField;
+    private JButton sub;
+    private JButton reset;
+    private JTextArea tout;
+    private JLabel resultText;
+
+    // constructor initializing the components
+    public SearchProgramFrame() {
+        setTitle("Calculate Pay Program"); // window title
+        setBounds(300, 200, 780, 500); // window position & size
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        c = getContentPane();
+        c.setLayout(null);
+
+        // form title
+        title = new JLabel("Calculate Weekly Pay");
+        title.setFont(new Font("Arial", Font.PLAIN, 30));
+        title.setSize(300, 30);
+        title.setLocation(240, 30);
+        c.add(title);
+
+        // labels
+        searchKey = new JLabel("Search");
+        searchKey.setFont(new Font("Arial", Font.PLAIN, 20));
+        searchKey.setSize(150, 30);
+        searchKey.setLocation(50, 100);
+        c.add(searchKey);
+
+        // fields
+        searchKeyField = new JTextField();
+        searchKeyField.setFont(new Font("Arial", Font.PLAIN, 20));
+        searchKeyField.setSize(190, 30);
+        searchKeyField.setLocation(180, 100);
+        c.add(searchKeyField);
+
+        // submit button
+        sub = new JButton("Submit");
+        sub.setFont(new Font("Arial", Font.PLAIN, 15));
+        sub.setSize(100, 20);
+        sub.setLocation(100, 300);
+        sub.addActionListener(this);
+        c.add(sub);
+
+        // reset button
+        reset = new JButton("Reset");
+        reset.setFont(new Font("Arial", Font.PLAIN, 15));
+        reset.setSize(100, 20);
+        reset.setLocation(220, 300);
+        reset.addActionListener(this);
+        c.add(reset);
+
+        // result text
+        resultText = new JLabel("");
+        resultText.setFont(new Font("Arial", Font.PLAIN, 20));
+        resultText.setSize(500, 25);
+        resultText.setLocation(50, 380);
+        c.add(resultText);
+
+        setVisible(true);
+    }
+
+    // validator
+    public Boolean validateIntegerInput(Integer input) {
+        return input != 0;
+    }
+
+    public Integer binarySearch(Integer numbers[], Integer l, Integer r, Integer v) {
+        if (r >= l) {
+            Integer mid = l + (r - l) / 2;
+
+            if (numbers[mid] == v) // check middle number
+                return mid;
+
+            // search left if the value is lower than the middle number
+            if (numbers[mid] > v)
+                return binarySearch(numbers, l, mid - 1, v);
+
+            // otherwise search the right of the middle number
+            return binarySearch(numbers, mid + 1, r, v);
+        }
+
+        // return false when there is no matching value
+        return -1;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        Integer searchKey = 0;
+        Integer numbers[] = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70 };
+        Integer size = numbers.length;
+
+        try {
+            Integer.parseInt(searchKeyField.getText());
+            searchKey = Integer.parseInt(searchKeyField.getText());
+        } catch (Exception x) {
+            searchKey = 0;
+        }
+
+        if (e.getSource() == sub) {
+            if (validateIntegerInput(searchKey)) {
+
+                Integer result = binarySearch(numbers, 0, size - 1, searchKey);
+                if (result == -1)
+                    resultText.setText("Element not present");
+                else
+                    resultText.setText("Element found at " + "index " + result);
+
+            } else {
+                tout.setText("");
+                resultText.setText("Please fill in a number.");
+            }
+        }
+        // reset button action
+        else if (e.getSource() == reset) {
+            String emtpy = "";
+            searchKeyField.setText(emtpy);
+        }
+    }
+}
+
+public class SearchProgram {
+    public static void main(String[] args) throws Exception {
+        SearchProgramFrame f = new SearchProgramFrame();
+        System.out.println(f != null ? "success" : "failed"); // added println for debugging purposes & avoid warning of
+                                                              // variable not being used
+    }
+}
