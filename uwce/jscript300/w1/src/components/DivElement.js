b2771362b1f0d7e8f57355e2b15468d94bc85713
Jun Kim <jun.kim@cdk.com>
2022-02-13 18:48:19 -0600

    importing backup files

---
+++ uwce/jscript300/w1/src/components/DivElement.js
@@ -0,0 +1,12 @@
+const HTMLElement = require('./HTMLElement');
+
+// Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content, content);
+    this.tag = 'div';
+  }
+}
+
+// Export class here
+module.exports = DivElement;
---
+++ uwce/jscript300/w1/src/timer/Timer.js
@@ -0,0 +1,19 @@
+// Defining class Timer
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
+}
+
+
+module.exports = Timer;
